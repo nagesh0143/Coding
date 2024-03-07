d7f380542900d7269919ab2c8892698768d617e6
nagesh0143 <139445392+nagesh0143@users.noreply.github.com>
2024-03-07 20:22:24 +0530

    Add files via upload

---
+++ data.js
@@ -0,0 +1,7 @@
+var data = {
+    "Nagesh": "nagesh@gmail.com",
+    "Asharaf": "asharaf@gmail.com",
+    "Chaitanya": "chaitanya@gmail.com",
+    "Mahesh": "mahesh@gmail.com",
+    "Sushant": "sushant@gmail.com"
+}
